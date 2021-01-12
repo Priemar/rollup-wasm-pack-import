383aa39480d35337873b5028e55533a028928f22
Mario <mario@priemar.dev>
2021-01-12 03:08:15 +0100

    changed (plugin) evaluation of resource and mapping key, based on the wasm package module added (plugin) new option serverPath updated (Readme) 2.0.0 changes

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ import wasmImport from 'rollup-wasm-pack-import';
     plugins: [        
         wasmImport({            
             copy: true,
+            serverPath: '/',
             mapping: {
                 'my-wasm-module': 'my-wasm.wasm'
             }
@@ -41,6 +42,7 @@ Property      | Type     | Description
 copy          | boolean  | if ``true``, the wasm will be copied in your output directory
 mapping       | object   | ``key:`` module name of your wasm-pack npm module<br>``value:`` the web assembly file name
 outputDir     | string   | if ``copy`` is set to true, the wasm files will be copied in this directory
+| serverPath  | string   | path from the serve url where the wasm file is located. The path should always must always end with '/'
 
 
 ### Your JS Code
@@ -61,6 +63,13 @@ async function load() {
 load();
 ```
 
+## Changes
+
+Version 2.0.0:
+- changed: The plugin is taking the ``name`` from the "wasm-pack" built module (package.json) and the name of the ``wasm`` file aswell to check against the ``mapping key``.
+- added: added a new option ``serverPath``
+
 
 ## Hint
-I wrote this plugin for my personal specific prototypíng use case. This means if you have slightly differnt requirements this plugin should be more like an inspiration which you can use and adapt. (currently there are no error check / tests its more like a poc)
+I wrote this plugin for my personal specific prototypíng use case. This means if you have slightly differnt requirements this plugin should be more like an inspiration which you can use and adapt. (currently there are no error check / tests its more like a poc).
+I am looking forward for a pull request with a cleaned version. :)
--- wasm-pack-import.js
+++ wasm-pack-import.js
@@ -1,4 +1,11 @@
+/**
+ * This plugin is a quick example (this is more a POC code) It need to be cleand up and some checks need to be added.
+ * I am looking forward for a pull request with a cleaned version. :)
+ */
+
 const fs = require('fs');
+const path = require('path');
+const { pathToFileURL } = require('url');
 
 /**
  * bundle wasm-pack node module. !!! wasm-pack output need to be from target type web !!!
@@ -7,35 +14,39 @@ const fs = require('fs');
  * @param {string} options.outputDir wasm output directory
  * @param {boolean} options.copy true: copy the was file into the output directory
  * @param {Object} options.mapping mapping to define output wasm file name (key = package-name, value: output wasm file name)
+ * @param {string} options.serverPath Directory on your server where the .wasm files will be loaded from. This is prepended to the URL, so you should put a / at the end of the directory, e.g. "/foo/".
  */
 function wasmPackImport(options) {
 
     // wasm resource files
     const resources = {};
+    options = options || {};
+    options.serverPath = options.serverPath || '';
 
     return {
         load: async (id) => {
-            let result = null;
-            const fileName = getJsFileName(id);
-            const map = options.mapping[fileName];
+            let result = null;            
+            const resourceInfo = await getResourceInfo(id);
+            const map = resourceInfo
+                        ? options?.mapping[resourceInfo.packageName]
+                        : null;
             if (map) {
-                if (options.copy) {
-                    const wasmSourceFile = id.replace(/^(.*\/[^\/]*)\.js$/, '$1_bg.wasm');
-                    const buffer = await fs.promises.readFile(wasmSourceFile);
+                if (options.copy) {                    
+                    const buffer = await fs.promises.readFile(resourceInfo.wasmFile);
                     resources[id] = {                    
                         output: options.outputDir ? `${options.outputDir}/${map}` : map,
                         buffer: buffer
                     };
                 }
-                let content = await fs.promises.readFile(id, 'utf8');
-                result = content.replace("import.meta.url.replace(/\\.js$/, '_bg.wasm');", `'${map}';`);                
+                let content = await fs.promises.readFile(id, 'utf8');                
+                result = content.replace("import.meta.url.replace(/\\.js$/, '_bg.wasm');", `'${options.serverPath}${map}';`);                
             }
             return result;
         },
         generateBundle: function() {
 
             return Promise.all(
-                Object.keys(resources).map(async id => {
+                Object.keys(resources).map(id => {
                     let file = resources[id];
                     return this.emitFile({
                         type: 'asset',
@@ -48,16 +59,43 @@ function wasmPackImport(options) {
     }
 }
 
-/**
- * get js file name
- * @param {string} id 
+/** get getResourceInfo
+ * @param {string} id
+ * @return {Promise<{ wasmFile: string, packageName: string }>} path of the wasm file name read from the package json in the folder
  */
-function getJsFileName(id) {
-    const rx = /([^\/]*).js$/;    
-    return rx.test(id)
-        ? id.match(rx)[1]
-        : null;
+async function getResourceInfo(id) {
+    let result = null;
+    try {
+        const dirName = path.dirname(id);
+        // check if there is a package file in the same directory
+        const packageJson = `${dirName}/package.json`;    
+        const hasPackageJson = await fileExists(packageJson);    
+        if (hasPackageJson) {
+            const packageJsonRaw = await fs.promises.readFile(packageJson, { encoding: 'utf8' });
+            const packageJsonItem = JSON.parse(packageJsonRaw);                        
+            const wasmFileName = packageJsonItem?.files?.find(name => name.endsWith('.wasm'));
+            if (wasmFileName?.length > 0 && packageJsonItem?.name?.length > 0) {
+                result = {
+                    wasmFile: `${dirName}/${wasmFileName}`,
+                    packageName: packageJsonItem.name,
+                };
+            }
+        }
+    }
+    catch(e) {
+        console.error(`Error while getting resource info for ${id}`, e);
+    }
+    return Promise.resolve(result);
 }
 
+/** check if file exists
+ * @param {string} path
+ * @return {Promise<boolean>} return tur if the file exists
+ */
+function fileExists(path) {
+    return fs.promises.access(path, fs.constants.F_OK)
+                    .then(() => true)
+                    .catch(() => false);
+}
 
 module.exports = wasmPackImport;
